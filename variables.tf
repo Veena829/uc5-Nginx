23451f737147c1ceed3f449321730255f00e1646
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-05-14 09:26:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,20 @@
 variable "aws_region" {
+  type    = string
   default = "us-east-1"
 }
 
-variable "instance_name" {}
-variable "instance_type" {}
-variable "ami_id" {}
-variable "key_name" {}
+variable "instance_name" {
+  type = string
+}
 
+variable "instance_type" {
+  type = string
+}
 
+variable "ami_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
